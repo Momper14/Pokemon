d259054fda8c353a563d895302728b351f68b989
zuppa93 <zuppa93@web.de>
2018-07-25 22:52:18 +0200

    a

--- Pokemon/attacks.c
+++ Pokemon/attacks.c
@@ -1,21 +1,33 @@
 #include "Pokemon.h"
 #include "Pokemon.c"
+#include "stdlib.h"
+#include "attacks.h"
+
 
 
 uint applyDMG(pokemonClass *angreifer, pokemonClass *verteidiger, Attacke *attacke) {
+
+	uint A = 0;
+	uint D = verteidiger->stats[verteidigung];
+	int random = 100 - (rand() % 16 + 1)
+	// ermitteln ob Angriff oder Spezialangriff
+	if (attacke->klasse == physisch) {
+		A = angreifer->stats[angriff];
+	}
+	else if (attacke->klasse == spezial) {
+		A = angreifer->stats[spezialangriff];
+	}
+	else {
+		A = 0;
+	}
 	/*
-			 ( (2*angreiferlevel )
-			 (------------------ )+ 2 *
-	Damage = (         5         )
-			 (
+	         ( (2*angreiferlevel )              (       A        )     )
+		     (------------------ )+ 2 * Power * (----------------)     )
+	Damage = (         5         )              (       D        )     )
+	         (----------------------------------------------------- + 2) * modifier  
+			 (                       50                                )
+
+	modifier = critical * random * STAB * type * burn * other
 	*/
-	uint damage = 0;
-	if (attacke.klasse == physisch) {
-		damage = angreifer->stats[angriff];
-	}
-	else if (attacke.klasse == spezial) {
-		damage = angreifer->stats[spezialangriff];
-	}
-	
 
 }
