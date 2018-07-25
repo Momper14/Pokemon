ff112b607305f11dfd6e6857428940773a25932f
zuppa93 <zuppa93@web.de>
2018-07-25 21:29:45 +0200

    added attack type class

---
+++ Pokemon/attacks.c
@@ -0,0 +1,10 @@
+#include "Pokemon.c"
+
+uint applyDMG(pokemonClass angreifer,pokemonClass, verteidiger, Attacke attacke) {
+	/*
+			 ( (2*angreiferlevel )
+			 (------------------ )+ 2 * 
+	Damage = (         5         )
+			 (
+	*/
+}
